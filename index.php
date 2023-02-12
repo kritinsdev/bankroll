c7ab9d7c894f787c1fbe9008f84409df9f30ad57
Kristaps Ritins <kritinsdev@gmail.com>
2023-02-13 00:42:22 +0200

    blocks

--- index.php
+++ index.php
@@ -2,7 +2,26 @@
 
 <main>
    <div class="container">
-      MAIN
+      <?php /** Blocks::displayAll(); */  ?>
+<?php
+
+// Check value exists.
+if( have_rows('blocks') ):
+    // Loop through rows.
+    while ( have_rows('blocks') ) : var_dump( the_row( true));
+
+        // Case: Paragraph layout.
+        if( get_row_layout() == 'block_content' ):
+
+        endif;
+
+    // End loop.
+    endwhile;
+
+// No value.
+else :
+    // Do something...
+endif; ?>
    </div>
 </main>
 
