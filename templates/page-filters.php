586642e75b29ed35335c26ccc585d2f0004128fd
Kristaps Ritins <kritinsdev@gmail.com>
2023-02-25 01:00:06 +0200

    filters template

---
+++ templates/page-filters.php
@@ -0,0 +1,14 @@
+<?php 
+/* Template Name: Page Filters */ 
+
+use Bankroll\Blocks\BlocksController;
+
+get_header(); ?>
+
+<main>
+   <div class="container">
+      <?php BlocksController::show(); ?>
+   </div>
+</main>
+
+<?php get_footer(); ?>
