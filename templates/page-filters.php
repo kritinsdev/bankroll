74fe3a178ef84a66279042e0688ca271f405a4cb
Kristaps Ritins <kritinsdev@gmail.com>
2023-03-02 00:54:04 +0200

    filters

--- templates/page-filters.php
+++ templates/page-filters.php
@@ -15,33 +15,99 @@ get_header(); ?>
                 <div class="filters">
                     <div class="filter filter-providers">
                         <div class="filter__header">
-                            Filter By Providers
+                            <span>Filter By Providers</span>
+                            <span>
+                                <svg width="8" height="5" viewBox="0 0 8 5">
+                                    <use xlink:href="#chevron-down"/>
+                                </svg>
+                            </span>
                         </div>
                         <div class="filter__data">
-                            <div>Netent</div>
-                            <div>Netent</div>
+                            <div class="filter__item">
+                                <input type="checkbox" id="checkbox-1">
+                                <label for="checkbox-1">Text for the checkbox</label>
+                            </div>
+
+                            <div class="filter__item">
+                                <input type="checkbox" id="checkbox-2">
+                                <label for="checkbox-2">Text for the checkbox</label>
+                            </div>
+
+                            <div class="filter__item">
+                                <input type="checkbox" id="checkbox-3">
+                                <label for="checkbox-3">Text for the checkbox</label>
+                            </div>
                         </div>
                     </div>
 
-                    <div class="filter filter-themes">
+                    <div class="filter filter-providers">
                         <div class="filter__header">
-                            Filter By Theme
+                            <span>Filter By Themes</span>
+                            <span>
+                                <svg width="8" height="5" viewBox="0 0 8 5">
+                                    <use xlink:href="#chevron-down"/>
+                                </svg>
+                            </span>
                         </div>
                         <div class="filter__data">
-                            <div>Netent</div>
-                            <div>Netent</div>
+                            <div class="filter__item">
+                                <input type="checkbox" id="checkbox-1">
+                                <label for="checkbox-1">Text for the checkbox</label>
+                            </div>
+
+                            <div class="filter__item">
+                                <input type="checkbox" id="checkbox-2">
+                                <label for="checkbox-2">Text for the checkbox</label>
+                            </div>
+
+                            <div class="filter__item">
+                                <input type="checkbox" id="checkbox-3">
+                                <label for="checkbox-3">Text for the checkbox</label>
+                            </div>
                         </div>
                     </div>
 
-                    <div class="filter filter-features">
+                    <div class="filter filter-providers">
                         <div class="filter__header">
-                            Filter By Features
+                            <span>Filter By Features</span>
+                            <span>
+                                <svg width="8" height="5" viewBox="0 0 8 5">
+                                    <use xlink:href="#chevron-down"/>
+                                </svg>
+                            </span>
                         </div>
-                        <div class="filter__data">
-                            <div>Netent</div>
-                            <div>Netent</div>
+                        <div class="filter__data show">
+                            <div class="filter__item">
+                                <input type="checkbox" id="checkbox-1">
+                                <label for="checkbox-1">Megaways</label>
+                            </div>
+
+                            <div class="filter__item">
+                                <input type="checkbox" id="checkbox-2">
+                                <label for="checkbox-2">Free Spins</label>
+                            </div>
+
+                            <div class="filter__item">
+                                <input type="checkbox" id="checkbox-3">
+                                <label for="checkbox-3">Pirates</label>
+                            </div>
+                            <div class="filter__item">
+                                <input type="checkbox" id="checkbox-1">
+                                <label for="checkbox-1">Megaways</label>
+                            </div>
+
+                            <div class="filter__item">
+                                <input type="checkbox" id="checkbox-2">
+                                <label for="checkbox-2">Free Spins</label>
+                            </div>
+
+                            <div class="filter__item">
+                                <input type="checkbox" id="checkbox-3">
+                                <label for="checkbox-3">Pirates</label>
+                            </div>
                         </div>
                     </div>
+
                 </div>
             </div>
         </div>
