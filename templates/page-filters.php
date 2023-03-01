892052b884edc542e89d90bb1952c806e29e67b8
Kristaps Ritins <kritinsdev@gmail.com>
2023-03-02 00:18:11 +0200

    filters template

--- templates/page-filters.php
+++ templates/page-filters.php
@@ -13,9 +13,29 @@ get_header(); ?>
             </div>
             <div class="mainArea__sidebar">
                 <div class="filters">
-                    <div class="filter">
+                    <div class="filter filter-providers">
                         <div class="filter__header">
-                            Filter By Provider
+                            Filter By Providers
+                        </div>
+                        <div class="filter__data">
+                            <div>Netent</div>
+                            <div>Netent</div>
+                        </div>
+                    </div>
+
+                    <div class="filter filter-themes">
+                        <div class="filter__header">
+                            Filter By Theme
+                        </div>
+                        <div class="filter__data">
+                            <div>Netent</div>
+                            <div>Netent</div>
+                        </div>
+                    </div>
+
+                    <div class="filter filter-features">
+                        <div class="filter__header">
+                            Filter By Features
                         </div>
                         <div class="filter__data">
                             <div>Netent</div>
