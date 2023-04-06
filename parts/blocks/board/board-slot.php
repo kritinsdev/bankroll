5032afedad7e9a619fb2e21aa7ab0772c070e470
Kristaps Ritins <kritinsdev@gmail.com>
2023-04-06 23:47:45 +0300

    slot style

--- parts/blocks/board/board-slot.php
+++ parts/blocks/board/board-slot.php
@@ -4,9 +4,15 @@ $image = $slot->getImage();
 $provider = $slot->getProvider();
 ?>
 
-<div class="board__item <?php if(isset($args['carousel'])) echo 'splide__slide'; ?> slot">
+<div class="board__item <?php if (isset($args['carousel']))
+    echo 'splide__slide'; ?> slot">
     <div class="slot__image">
         <img src="<?php echo $image['url']; ?>" alt="">
     </div>
-
+    <div class="slot__details">
+        <div>
+            <div class="slot__title"><?php echo $slot->getName(); ?></div>
+            <div class="slot__provider"><?php foreach($provider as $prov) echo $prov; ?></div>
+        </div>
+    </div>
 </div>
