7583759be9422fd9bd0c9cd02a61d1e4b9bd21f7
Kristaps Ritins <kritinsdev@gmail.com>
2023-04-10 23:37:53 +0300

    slot styles

--- parts/global/hero/hero-slot.php
+++ parts/global/hero/hero-slot.php
@@ -3,7 +3,7 @@ use Bankroll\Includes\Factory\SlotFactory;
 use Bankroll\Includes\View\Parts;
 
 $slot = SlotFactory::create(get_the_ID());
-$imageUrl = $slot->getImage()['sizes']['medium'];
+$imageUrl = ($slot->getImage()) ? $slot->getImage()['sizes']['medium'] : '';
 ?>
 
 <div class="hero slot">
@@ -25,10 +25,25 @@ $imageUrl = $slot->getImage()['sizes']['medium'];
             </div>
 
             <div class="hero__slotInfo">
-                <h2>Slot info</h2>
                 <div class="infoItem">
-                    <div>RTP</div>
+                    <span>RTP</span>
                     <div><?php echo $slot->getRtp(); ?></div>
+                    <i class="fa-solid fa-percent fa-xl"></i>
+                </div>
+                <div class="infoItem">
+                    <span>Max Multi</span>
+                    <div>x<?php echo $slot->getMaxMultiplier(); ?></div>
+                    <i class="fa-solid fa-coins fa-xl"></i>
+                </div>
+                <div class="infoItem">
+                    <span>Release date</span>
+                    <div>24.08.2018</div>
+                    <i class="fa-solid fa-calendar-days fa-xl"></i>
+                </div>
+                <div class="infoItem">
+                    <span>Something</span>
+                    <div>Best slot ever</div>
+                    <i class="fa-solid fa-sack-dollar fa-xl"></i>
                 </div>
             </div>
         </div>
