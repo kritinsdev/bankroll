6f05cb5c3ccc111661bc33fed00d475a6c4d49d8
Kristaps Ritins <kritinsdev@gmail.com>
2023-06-08 00:04:56 +0300

    css changes

--- parts/cards/slot/card-1.php
+++ parts/cards/slot/card-1.php
@@ -8,22 +8,26 @@ $rtp = $slot->getRtp();
 // $rtpRange = Helpers::getSlotRtpRange($rtp);
 ?>
 
-<div class="board__item <?php if (isset($args['carousel'])) echo 'splide__slide'; ?> slot">
-    <?php if(!empty($image['url'])) : ?>
+<div class="board__item slot">
+    <?php if (!empty($image['url'])): ?>
         <div class="slot__image">
             <img src="<?php echo $image['url']; ?>" alt="">
         </div>
-    <?php else : ?>
+    <?php else: ?>
         <div class="slot__image blank">
             IMAGE MISSING
         </div>
     <?php endif; ?>
     <div class="slot__details">
-            <a href="<?php echo $slot->getPermalink(); ?>" class="slot__title"><?php echo $slot->getName(); ?></a>
-            <div class="slot__sub">
-                <?php foreach($provider as $prov) : ?>
-                    <?php echo $prov; ?>
-                <?php endforeach; ?>
-            </div>
+        <a href="<?php echo $slot->getPermalink(); ?>" class="slot__title"><?php echo $slot->getName(); ?></a>
+        <div class="slot_rtp">
+            <span>RTP</span>
+            <span>97.06%</span>
+        </div>
     </div>
+    <div class="slot__cta">
+        <a href="#">Play now</a>
+    </div>
+
+
 </div>
