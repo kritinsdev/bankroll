803aef1976639cc6cfdda4594aa32be58463185c
Kristaps Ritins <kritinsdev@gmail.com>
2024-02-28 23:10:57 +0200

    some layout styles

--- Blocks/Content/view.php
+++ Blocks/Content/view.php
@@ -1,5 +1,3 @@
-<?php dump($args, 1); ?>
-
 <div>
     <?php echo $args['block_content_editor']; ?>
 </div>
--- footer.php
+++ footer.php
@@ -1,6 +1,17 @@
 <footer class="footer">
     <div class="container">
-        FOOTER
+        <div class="footer-content">
+            <div class="footer-menu">
+                <div class="footer-menu__title">Payment Methods</div>
+                <ul class="footer-menu__items">
+                    <li>All payment methods</li>
+                    <li>Paypal</li>
+                    <li>Neteller</li>
+                    <li>Visa</li>
+                    <li>Bitcoin</li>
+                </ul>
+            </div>
+        </div>
     </div>
 </footer>
 
