9cd0f35085144574bf4bd9c1fdfea95db9c6c918
Kristaps Ritins <kritinsdev@gmail.com>
2024-03-10 23:19:45 +0200

    changes

--- Blocks/Toplist/resources/templates/template-1.php
+++ Blocks/Toplist/resources/templates/template-1.php
@@ -11,6 +11,9 @@ $toplist_item = $args;
     <div>
         <img class="testimg" src="<?php echo $toplist_item['image']['url']; ?>" alt="">
     </div>
+    <div>
+        <?php print_r($toplist_item['main_bonus']); ?>
+    </div>
 </div>
 
 <style>
