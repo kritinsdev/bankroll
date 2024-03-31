e4ee6eb71e4fdfdb7ccdc2c15b887b5c393b8946
Kristaps Ritins <kritinsdev@gmail.com>
2024-04-01 00:19:44 +0300

    tweaks

--- parts/hero/page.php
+++ parts/hero/page.php
@@ -4,20 +4,32 @@ use Bankroll\Includes\View\Components;
 
 $hero = $args;
 
+echo '<pre>';
+
+print_r($hero);
+
+echo '</pre>';
+
 ?>
 <div class="hero" style="background-image:url('<?php echo $hero['background_image'][0]; ?>')">
-    <div class="hero-wrap">
-        <div class="hero-main">
-            <h1 class="hero-title">
-                <?php echo $hero['title']; ?>
-            </h1>
-            <div class="hero-text">
-                <?php echo $hero['text']; ?>
+    <div class="hero__wrap">
+        <div class="hero__main">
+            <div class="hero__info">
+                <h1 class="hero__title">
+                    <?php echo $hero['title']; ?>
+                </h1>
+                <div class="hero__text">
+                    <?php echo $hero['text']; ?>
+                </div>
             </div>
 
-            <div>
+            <div class="hero__extra">
                 <?php Components::image($hero['image']); ?>
             </div>
+        </div>
+
+        <div>
+
         </div>
     </div>
 </div>
